17908a6366c94aacbda15d164df6da4018f3adc2
tilek <saparovtilek626@gmaill.com>
2023-08-01 19:28:28 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+# git - это система управления версиями кода
+# github - сайт корорый дает хранилище для кода
+# так же есть gitlab, bitbucket
+
+# репозиторий - папка с проектом с которой может работать git
+
+# ssh-keygen - созданиие ключа
+
+# git config --global user.name 'your name'
+# git config --global user.email 'your email'
+#  указание своих данных
+
+
+# git init - превратить папку в репозиторий и создает скрытую паку git 
